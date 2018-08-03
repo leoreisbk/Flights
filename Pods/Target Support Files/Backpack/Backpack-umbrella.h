305fe75e9536198cc25cef5cbc7af5156ad6dcf5
Leonardo Reis <leo.reis@b2wdigital.com>
2018-08-03 18:01:04 -0300

    fix

---
+++ Pods/Target Support Files/Backpack/Backpack-umbrella.h
@@ -0,0 +1,29 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Backpack.h"
+#import "Color.h"
+#import "BPKColor.h"
+#import "Font.h"
+#import "BPKFont.h"
+#import "BPKGradient.h"
+#import "Gradient.h"
+#import "BPKRadii.h"
+#import "Radii.h"
+#import "BPKShadow.h"
+#import "Shadow.h"
+#import "BPKSpacing.h"
+#import "Spacing.h"
+
+FOUNDATION_EXPORT double BackpackVersionNumber;
+FOUNDATION_EXPORT const unsigned char BackpackVersionString[];
+
